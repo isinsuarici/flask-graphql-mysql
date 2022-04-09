05a46b24184bf2f6d4f1fbe4de294d9d67d0740a
Işınsu Arıcı <isinsuu98@hotmail.com>
2022-04-09 06:48:57 +0300

    init added

---
+++ api/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] =  'mysql://root:*******@localhost/cafeprojesi'
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'My First API !!'
